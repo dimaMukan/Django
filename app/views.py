5e495dba31511b10eb27435bf77b98076eda4552
dimaMukan <ds.mukan@gmail.com>
2023-04-19 15:01:09 +0200

    sql + urls

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
@@ -7,30 +7,30 @@ from .models import Movie
 
 def name(request):
     return HttpResponse("123")
-
-@dataclass
-class Person:
-    name:str
-    age:int
-
-    def __str__(self):
-        return f'This is {self.name}'
-
-
-info_dict = {
-    "one":"ONE!",
-    "two":"TWO!",
-    "three":"THREE!",
-    "four":"FOUR!",
-    "five":"FIVE!",
-}
-
-# def hello(request):
-#     numbers = list(info_dict)
-#     context = {
-#         'numbers':numbers
-#     }
-#     return render(request,'app/test.html',context=context)
+#
+# @dataclass
+# class Person:
+#     name:str
+#     age:int
+#
+#     def __str__(self):
+#         return f'This is {self.name}'
+#
+#
+# info_dict = {
+#     "one":"ONE!",
+#     "two":"TWO!",
+#     "three":"THREE!",
+#     "four":"FOUR!",
+#     "five":"FIVE!",
+# }
+#
+# # def hello(request):
+# #     numbers = list(info_dict)
+# #     context = {
+# #         'numbers':numbers
+# #     }
+# #     return render(request,'app/test.html',context=context)
 
 def get_info(request):
     movies = Movie.objects.all()
@@ -39,9 +39,9 @@ def get_info(request):
     })
 
 def get_info_about_one(request,id_movie:int):
-    movies = Movie.objects.all()
-    return render(request,'app/info.html',{
-        'movies':movies
+    movie = get_object_or_404(Movie,id=id_movie)
+    return render(request,'app/info_one.html',{
+        'movie':movie
     })
 
 #
