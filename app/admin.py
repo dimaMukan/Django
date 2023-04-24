0a9019c3c05b59e2cc7775f74d1c6d1ead0bc4fe
dimaMukan <ds.mukan@gmail.com>
2023-04-25 00:55:00 +0200

    some more changes

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,60 @@
 from django.contrib import admin
 from .models import Movie
+from django.db.models import QuerySet
 
-admin.site.register(Movie)
+
+class RatingFilter(admin.SimpleListFilter):
+    title = "Movie by rating"
+    parameter_name = 'qwe'
+
+    def lookups(self,request,model_admin):
+        return [
+                ('<50','low'),
+                ('50-70','middle'),
+                ('70-90','ok'),
+                ('90+','perfect'),
+        ]
+
+    def queryset(self,request,queryset:QuerySet):
+        if self.value()=='<50':
+            return queryset.filter(rating__lt=50)
+        if self.value()=='50-70':
+            return queryset.filter(rating__gte=50).filter(rating__lt=70)
+        if self.value()=='70-90':
+            return queryset.filter(rating__gte=70).filter(rating__lt=90)
+        # if self.value()=='90+':
+        #     return qs.filter(rating__gte=90)
+        return queryset
+
+
+
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+
+# !!!!!!!!!!!
+    prepopulated_fields = {'slug':('name',)}
+# !!!!!!!!!!!
+    list_display = ['name','rating','currency','rating_status']
+    list_editable = ['rating','currency']
+    readonly_fields = ['rating']
+    ordering = ['-rating']
+    list_per_page = 7
+    actions = ['set_dollar']
+    search_fields = ['name','rating']
+    list_filter = ['name',RatingFilter]
+
+    @admin.display(description='Status')
+    def rating_status(self,mov:Movie):
+        if mov.rating < 60:
+            return 'Bad'
+        if mov.rating < 75:
+            return 'Not bad'
+        return 'Good'
+
+    @admin.action(description='All in USD')
+    def set_dollar(self,request,qs:QuerySet):
+        count = qs.update(currency=Movie.Usd)
+        self.message_user(request,f'Was changed {count} ')
+
--- app/models.py
+++ app/models.py
@@ -1,10 +1,22 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Movie(models.Model):
+
+    Euro = 'EUR'
+    Usd = 'USD'
+    Uh = 'UKG'
+    Currency = [
+        (Euro, 'Euro'),
+        (Usd, 'Dollar'),
+        (Uh, 'Hrivna')
+    ]
+
+    currency = models.CharField(max_length=3,choices=Currency,default=Usd)
     name=models.CharField(max_length=40)
-    rating=models.IntegerField()
+    rating=models.IntegerField(null=True,blank=True, validators=[MinValueValidator(1),MaxValueValidator(100)])
     slug = models.SlugField(default='',null=False,db_index=True)
 
     def save(self,*args,**kwargs):
